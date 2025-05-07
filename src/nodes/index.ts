3a741e762476efa312297c7167e13c0997fe4172
Balint Mate <mate@fzi.de>
2025-05-07 16:17:41 +0200

    feed data from json

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -3,23 +3,10 @@ import type { NodeTypes } from '@xyflow/react';
 import { PositionLoggerNode } from './PositionLoggerNode';
 import { AppNode } from './types';
 
-export const initialNodes: AppNode[] = [
-  { id: 'a', type: 'input', position: { x: 0, y: 0 }, data: { label: 'wire' } },
-  {
-    id: 'b',
-    type: 'position-logger',
-    position: { x: -100, y: 100 },
-    data: { label: 'drag me!' },
-  },
-  { id: 'c', position: { x: 100, y: 100 }, data: { label: 'your ideas' } },
-  {
-    id: 'd',
-    type: 'output',
-    position: { x: 0, y: 200 },
-    data: { label: 'with React Flow' },
-  },
-  { id: 'e', position: { x: 0, y: 300 }, data: { label: 'just added' } },
-];
+// Import nodes from JSON
+import rawNodes from './nodes.json';
+
+export const initialNodes = rawNodes as unknown as AppNode[];
 
 export const nodeTypes = {
   'position-logger': PositionLoggerNode,
